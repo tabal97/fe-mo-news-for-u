3906a67913cf077e55fa641f5c6c556346a8e395
Mohamed <mr.tabal@yahoo.co.uk>
2019-10-11 13:10:25 +0100

    completed infinite scrolling

--- mo-news-for-u/src/utils/api.js
+++ mo-news-for-u/src/utils/api.js
@@ -7,8 +7,8 @@ export const getTopics = async () => {
     return data;
 }
 
-export const getArticles = async (topic, sortBy, orderBy) => {
-    const { data } = await request.get("/articles", { params: { topic, sortBy, orderBy } });
+export const getArticles = async (topic, sortBy, orderBy, p) => {
+    const { data } = await request.get("/articles", { params: { topic, sortBy, orderBy, p } });
     return data
 }
 
